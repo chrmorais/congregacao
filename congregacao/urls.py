47f8d1781a978ece9472cd18f1190c3921fd3e14
Ricardo Duarte <ricardo.duarte@petrobras.com.br>
2013-12-20 15:27:27 -0200

    Incluido o cabecalho do sistema

--- congregacao/urls.py
+++ congregacao/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from main.views import IndexView
+
 urlpatterns = patterns('',
-   (r'^accounts/', include('registration.backends.default.urls')),
-    # Examples:
-    # url(r'^$', 'congregacao.views.home', name='home'),
+    (r'^accounts/', include('registration.backends.default.urls')),
+
+    url(r'^$', IndexView.as_view(), name='index'),
     # url(r'^congregacao/', include('congregacao.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- main/views.py
+++ main/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals, print_function
+
+from django.views.generic import TemplateView
+
+class IndexView(TemplateView):
+    template_name = 'main/index.html'
+
+
